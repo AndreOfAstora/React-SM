85946a3fb76f038a9519e322d82b24bb90922120
Andrew <you@example.com>
2021-10-09 19:02:52 +0300

    Lesson 59. Profile page HTTP support.
     Changes:
        - ProfileAPIComponnent now sends http request;
        - SET_PPROFILE_INFO action and it's support added to profileReducer.js.

--- src/Profile/ProfileAPIComponent.jsx
+++ src/Profile/ProfileAPIComponent.jsx
@@ -1,8 +1,18 @@
+import axios from 'axios';
 import React from 'react';
 import Profile from './Profile';
 
+window.response = {};
 
 class ProfileAPIComponent extends React.Component {
+
+
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/profile/2').then(response => { window.response = {...response};
+        });        
+    }
+
+
     render() {
         return (
             <div>
@@ -11,5 +21,8 @@ class ProfileAPIComponent extends React.Component {
         )
     }
 }
+
+
+
 
 export default ProfileAPIComponent;
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,18 +1,19 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
+const SET_PROFILE_INFO = 'SET_PROFILE_INFO';
+
 let initialState = {
   postData: [
     { link: "https://www.freeiconspng.com/uploads/gucci-logo-hd-picture-free-download-0.png", text: "Traversing software documentation is hard, use copybook and mind maps to make it easy.", color: '#308816' },
     { link: 'https://www.freeiconspng.com/uploads/gucci-logo-hd-picture-free-download-0.png', text: "Here's how simple rest and frequent breaks have an NZT effect on human brain.", color: 'red' }
   ],
-  newPostText: ''
+  newPostText: '',
+
+  profileInfo: {}
 }
 
-export const addPostActionCreator = () => ( { type: ADD_POST } );
 
-export const updateNewPostTextActionCreator = (text) => 
-        ({ type: UPDATE_NEW_POST_TEXT, newText: text });
 
 const profileReducer = (state = initialState, action) =>{
 
@@ -37,10 +38,28 @@ const profileReducer = (state = initialState, action) =>{
               newPostText: action.newText
             }
         }
+
+        case SET_PROFILE_INFO: {
+            return {
+                ...state,
+                profileInfo: {...action.profileInfo}
+            }
+        }
+
         default:
             return state;
     }
     
 } 
+
+
+export const addPostActionCreator = () => ( { type: ADD_POST } );
+
+export const updateNewPostTextActionCreator = (text) => 
+        ({ type: UPDATE_NEW_POST_TEXT, newText: text });
+
+
+export const setProfileInfo = (profileInfo) => ( { type: SET_PROFILE_INFO, profileInfo } );
+
 
 export default profileReducer;
